fb7b83de7c6990e6c7bf0cddd5ab82b1ab175765
GAYATHRI592 <111274169+GAYATHRI592@users.noreply.github.com>
2022-10-01 10:47:47 +0530

    uploads

--- Assignment/Bavani/Assignment-1/oddnum.py
+++
@@ -1,7 +0,0 @@
-m=int(input("enter the starting number:"))
-n=int(input("enter the ending number:"))
-i=m
-while(i<=n):
-    if(i%2!=0):
-        print(i)
-    i+=1
--- Assignment/Gayathri/Assignment-1/odd.py
+++
@@ -1,6 +0,0 @@
-
-maximum = int(input(" Please Enter any Maximum Value : "))
-
-for number in range(1, maximum + 1):
-    if(number % 2 != 0):
-        print(number)
